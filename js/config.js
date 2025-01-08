d456e8538b81b42b6c84be7b68cec26ad2982b20
value <paddygermaine@hotmail.co.uk>
2025-01-08 15:46:20 +0000

    - test

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export async function loadConfig() {
-    const response = await fetch('../credentials.json');
+    const response = await fetch('credentials.json');
     if (!response.ok) throw new Error('Failed to fetch config');
     return response.json();
 }
